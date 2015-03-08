c485c50b7a469a1dc81c122a97fed43032b7fe81
René Birkeland <renesbirkeland@gmail.com>
2015-03-08 20:22:50 +0000

    wp

--- src/ScreensController.java
+++ src/ScreensController.java
@@ -58,14 +58,14 @@ public class ScreensController  extends StackPane {
             if (!getChildren().isEmpty()) {    //if there is more than one screen
                 Timeline fade = new Timeline(
                         new KeyFrame(Duration.ZERO, new KeyValue(opacity, 1.0)),
-                        new KeyFrame(new Duration(100), new EventHandler<ActionEvent>() {
+                        new KeyFrame(new Duration(100), new EventHandler<ActionEvent>() { //Fade out
                             @Override
                             public void handle(ActionEvent t) {
                                 getChildren().remove(0);                    //remove the displayed screen
                                 getChildren().add(0, screens.get(name));     //add the screen
                                 Timeline fadeIn = new Timeline(
                                         new KeyFrame(Duration.ZERO, new KeyValue(opacity, 0.0)),
-                                        new KeyFrame(new Duration(800), new KeyValue(opacity, 1.0)));
+                                        new KeyFrame(new Duration(100), new KeyValue(opacity, 1.0))); //Fade in
                                 fadeIn.play();
                             }
                         }, new KeyValue(opacity, 0.0)));
@@ -76,7 +76,7 @@ public class ScreensController  extends StackPane {
                 getChildren().add(screens.get(name));       //no one else been displayed, then just show
                 Timeline fadeIn = new Timeline(
                         new KeyFrame(Duration.ZERO, new KeyValue(opacity, 0.0)),
-                        new KeyFrame(new Duration(2500), new KeyValue(opacity, 1.0)));
+                        new KeyFrame(new Duration(1), new KeyValue(opacity, 1.0))); //First fade in
                 fadeIn.play();
             }
             return true;
