e58778e018a151d677274e1e186cb145cfae278d
Filip-Ter <filip.ter@gmail.com>
2015-03-09 17:52:48 +0000

    Solving Renes stupid button problem :P

--- src/Screen1Controller.java
+++ src/Screen1Controller.java
@@ -1,6 +1,10 @@
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
@@ -13,10 +17,13 @@ import java.util.ResourceBundle;
 public class Screen1Controller implements Initializable, ControlledScreen {
 
     ScreensController myController;
+    @FXML private Button toggle;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        Image buttonImg = new Image(getClass().getResourceAsStream("img/btnimg.png"), 59, 27, false, true);
+        toggle.setGraphic(new ImageView(buttonImg));
     }
 
     public void setScreenParent(ScreensController screenParent) {
